162939b33102092ff2c4f2db825d8d3a51987176
Marcos Júnior Lemes Ferreira <marcos.j.lemes.2004@gmail.com>
2025-07-24 22:28:27 +0000

    feat: initial style

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const imgSrc = './images/dark_mode_25dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.png'
+
+document.addEventListener('DOMContentLoaded', function() {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const body = document.body;
+    
+    // Check for saved user preference
+    if (localStorage.getItem('darkMode') === 'enabled') {
+        body.classList.add('dark-mode');
+        darkModeToggle.innerHTML = '<span class="toggle-icon">☀️</span>';
+    }
+    
+    // Toggle dark mode
+    darkModeToggle.addEventListener('click', function() {
+        body.classList.toggle('dark-mode');
+        
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('darkMode', 'enabled');
+            darkModeToggle.innerHTML = '<span class="toggle-icon">☀️</span>';
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+            // darkModeToggle.innerHTML = `<span class="toggle-icon"><img src="${imgSrc}" alt="Sun icon" /></span>`;
+
+            darkModeToggle.innerHTML = '<span class="toggle-icon">🌙</span>';
+        }
+    });
+});
