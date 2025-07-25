57c8d099a0e04cca48ed70eff2d7beb0a5b64c6a
Marcos Júnior Lemes Ferreira <marcos.j.lemes.2004@gmail.com>
2025-07-25 15:54:31 +0000

    feat: links and networks added in the footer

--- js/script.js
+++ js/script.js
@@ -25,3 +25,19 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+function copyEmail() {
+    const emailInput = document.getElementById("email-ifsc");
+    emailInput.select();
+    emailInput.setSelectionRange(0, 99999); // Para dispositivos móveis
+    document.execCommand("copy");
+    alert("Email copiado: " + emailInput.value);
+}
+                
+function copyEmailP(){
+    const emailInput = document.getElementById("email-personal");
+    emailInput.select();
+    emailInput.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    alert("Email copiado: " + emailInput.value);
+}
